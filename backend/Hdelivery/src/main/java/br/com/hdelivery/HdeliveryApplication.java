e7659602d0004122c2682a42f5998d724228c670
Cezar Augusto <cezar.ags@icloud.com>
2021-01-10 11:40:29 -0300

    Projeto Criado

---
+++ backend/Hdelivery/src/main/java/br/com/hdelivery/HdeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.hdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdeliveryApplication.class, args);
+	}
+
+}
